50d905b01aaf853db9b643625076f114a46d1274
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-02-09 12:29:12 +0530

    Create 1. SecondLargest.java

---
+++ Arrays/1. SecondLargest.java
@@ -0,0 +1,23 @@
+class Solution
+{ 
+    public static int print2largest(int A[],int N) 
+    {
+        
+        //CODE HERE
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for(int i=0; i<N; i++){
+            if(A[i]>first){
+                second = first;
+                first = A[i];
+            }
+            else if(A[i]>second && A[i] != first)  // This is used for finding the element between first and second 
+                second = A[i];
+        }
+        
+        if(second == Integer.MIN_VALUE) // if there is no second largest it simply returns to -1
+            return -1;
+        return second;
+    }
+}
+        
