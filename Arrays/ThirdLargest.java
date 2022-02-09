cebf426cb6899d1cbbe64c6469ce82894e372647
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-02-09 17:12:19 +0530

    Create ThirdLargest.java

---
+++ Arrays/ThirdLargest.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    int thirdLargest(int a[], int n)
+    {
+	    // Your code here
+	    int first = a[0];
+	    int second = Integer.MIN_VALUE;
+	    int third = Integer.MIN_VALUE;
+	    
+	    for(int i=1; i<a.length; i++){   
+	        if(a[i]>first){
+	            third = second;
+	            second = first;
+	            first = a[i];
+	        }
+	        
+	        else if(a[i]>second){  // check for element greater than second but less than first
+	            third = second;
+	            second = a[i];
+	            }
+	            
+            else if(a[i]>third){   // check for element greater than third but less than first and second
+                third = a[i];
+                }
+	   }
+	   
+	   if(third == Integer.MIN_VALUE)  // if third largest not exits simply return -1
+	        return -1;
+	    
+	    return third;
+    }
+}
