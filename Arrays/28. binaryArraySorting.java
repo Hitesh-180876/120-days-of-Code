68b1c7523fc1d35e869a4bae8f9973d769120ae9
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-02-12 23:29:42 +0530

    Update 28. binaryArraySorting.java

--- Arrays/28. binaryArraySorting.java
+++ Arrays/28. binaryArraySorting.java
@@ -1,1 +1,24 @@
 
+package arrays;
+
+public class removeDuplicates {
+	public static void main(String[] args) {
+		int[] arr = new int[] {1, 0, 0, 1, 0, 1};
+		int n =6;
+		int setZero = 0;
+		for(int i=0; i< arr.length; i++){
+		    if(arr[i] == 0){                 // swap each zero with the current 1
+		        int temp = arr[i];          
+		        arr[i]=arr[setZero];                 
+		        arr[setZero]=temp;
+		        setZero++;
+		    }
+		 
+		 }
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		
+	}
+	}
+
+}
