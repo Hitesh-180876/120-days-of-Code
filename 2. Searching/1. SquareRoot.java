4b97a8459eb43c4e6af95a16276f8f57ac116222
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-02-15 22:50:49 +0530

    Update 1. SquareRoot.java

--- 2. Searching/1. SquareRoot.java
+++ 2. Searching/1. SquareRoot.java
@@ -1,1 +1,16 @@
 
+import java.util.*;
+ 
+class SquareRoot {
+    static int countSquares(int x) {
+        int sqr = (int) Math.sqrt(x);
+        int result = (int) (sqr);
+        return result;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int x = 9;
+        System.out.print(countSquares(x));
+    }
+}
