0786ba872ff43ee9ff8445a9b1e913a06d7cd827
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-03 18:11:32 +0530

    Update ArrayRemoveConsecutive.java

--- LinkedList-1/ArrayRemoveConsecutive.java
+++ LinkedList-1/ArrayRemoveConsecutive.java
@@ -1,1 +1,32 @@
+package linkedList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayListRemoveConsecutiveDuplicates {
+	
+	public static ArrayList<Integer> removeDup(int arr[]){
+		ArrayList<Integer> array = new ArrayList<>(4);
+		
+		array.add(arr[0]);
+		
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i] != arr[i-1]) {
+				array.add(arr[i]);
+			}
+		}
+		
+		
+		return array;
+		
+	}
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 3, 3, 3, 5, 6 , 3};
+		ArrayList<Integer> ans = removeDup(arr);
+		for(int i:ans) {
+			System.out.print(i + " ");
+		}
+		
+		
+	}
+
+}
