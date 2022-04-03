3f61af3378ddf16c918f898d8b3112b210fe2cb0
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-03 17:46:38 +0530

    Update 1.ArrayList.java

--- LinkedList-1/1.ArrayList.java
+++ LinkedList-1/1.ArrayList.java
@@ -7,8 +7,18 @@ public class ArrayListImplementation {
 		arr.add(20);
 		arr.add(1, 30);
 		arr.add(2, 40);
+		Integer o = 10;
+		arr.add(o);
+		
+		for(int i=0; i<arr.size(); i++) {
+			System.out.println(arr.get(i));
+		}
+		
+		for(int i: arr) {
+			System.out.println(i);
+		}
+		
 		System.out.println(arr.size());
 	}
 
 }
-
