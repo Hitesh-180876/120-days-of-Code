0b0f1f40f50d1143507570d2e4e54b7d9111f2e5
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-03 23:46:00 +0530

    Create TakeLinkedListInput.java

---
+++ LinkedList-1/TakeLinkedListInput.java
@@ -0,0 +1,57 @@
+package linkedList1;
+
+public class Node {
+	int data;
+	Node next;
+	
+	Node(int data){
+		this.data = data;
+	}
+
+}
+
+
+
+package linkedList1;
+
+import java.util.Scanner;
+
+public class LinkedListInput {
+	public static void printLL(Node head) {
+		while(head != null) {
+			System.out.print(head.data+"->");
+			head = head.next;
+		}
+	}
+	public static Node takeInput() {
+		Scanner sc = new Scanner(System.in);
+		
+		Node head = null, tail = null;
+		int data = sc.nextInt();
+		
+		while(data != -1) {
+			Node currNode = new Node(data);
+			
+			if(head == null) {
+				head = currNode;
+				tail = currNode;
+			}else {
+				tail.next = currNode;
+				tail = currNode;
+			}
+			data = sc.nextInt();
+			
+		
+		}
+		sc.close();
+		return head;
+		
+		
+	}
+	public static void main(String[] args) {
+		Node head = takeInput();
+		printLL(head);
+		
+	}
+	
+}
