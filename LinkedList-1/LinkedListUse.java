cdd25e664353e2620dcc854ff94abb4c06024e13
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-03 21:05:26 +0530

    Update LinkedListUse.java

--- LinkedList-1/LinkedListUse.java
+++ LinkedList-1/LinkedListUse.java
@@ -1,1 +1,33 @@
 
+package linkedList1;
+
+public class LinkedListUse {
+	public static void incrementLL(Node  head) {
+		while(head != null) {
+			head.data++;
+			head = head.next;
+		}
+	}
+	public static Node createLL() {
+		Node n1 = new Node(30);
+		Node n2 = new Node(40);
+		Node n3 = new Node(50);
+		n1.next = n2;
+		n2.next = n3;
+		
+		return n1;
+		
+	}
+	public static void main(String[] args) {
+		
+		Node head = createLL();
+		incrementLL(head);
+		
+		while(head!=null) {
+			System.out.print(head.data+"->");
+			head = head.next;
+		}
+		System.out.print("null");
+	}
+
+}
