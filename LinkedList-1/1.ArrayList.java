ee985fb98dee5c5dd3fd6a62e4b9a0ecacac4e1c
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-03 17:32:02 +0530

    Update 1.ArrayList.java

--- LinkedList-1/1.ArrayList.java
+++ LinkedList-1/1.ArrayList.java
@@ -1,1 +1,14 @@
+package linkedList;
+import java.util.ArrayList;
+
+public class ArrayListImplementation {
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(20);
+		arr.add(1, 30);
+		arr.add(2, 40);
+		System.out.println(arr.size());
+	}
+
+}
 
