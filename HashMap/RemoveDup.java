ae0247c536be2f621ff84069e9007706c9a7c81f
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-07 11:56:38 +0530

    Update RemoveDup.java

--- HashMap/RemoveDup.java
+++ HashMap/RemoveDup.java
@@ -1,1 +1,32 @@
+/* remove Duplicate elements from array using HashMap */
 
+
+
+package hashMap;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class RemoveDup {
+	public static ArrayList<Integer> uniqueEle(int[] arr){
+		ArrayList<Integer> output = new ArrayList<>();
+		HashMap<Integer, Boolean> map = new HashMap<>();
+		
+		for(int i =0; i<arr.length; i++) {
+			if(map.containsKey(arr[i])) {
+				continue;
+			}
+			output.add(arr[i]);
+			map.put(arr[i], true);
+		}
+		
+		return output;
+		
+	}
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 4, 1, 2, 3, 4};
+		ArrayList<Integer> output = uniqueEle(arr);
+		System.out.println(output);
+		
+	}
+
+}
