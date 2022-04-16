32c076a92d62e9d19753c47351dcd7db59b2c494
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-17 00:17:38 +0530

    Update greaterThanX.java

--- Tree-1/greaterThanX.java
+++ Tree-1/greaterThanX.java
@@ -1,1 +1,17 @@
-
+	public static int greaterThanX(Node root, int x) {
+		if(root == null) {
+			return 0;
+		}
+		int count = root.data>x ? 1:0;
+		
+		if(root.left!=null) {
+			count+= greaterThanX(root.left, x);
+		}
+		
+		if(root.right !=null) {
+			count+=greaterThanX(root.right, x);
+		}
+		
+		return count;
+		
+	}
