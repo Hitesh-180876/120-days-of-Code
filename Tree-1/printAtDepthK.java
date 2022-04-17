1c2c89ddb0c1cb1214a755924f7714ee59ffbf2a
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-17 17:28:40 +0530

    Create printAtDepthK.java

---
+++ Tree-1/printAtDepthK.java
@@ -0,0 +1,10 @@
+public static void printAtDepthK(Node root, int k) {
+		if(root == null) {
+			return;
+		}
+		if(k == 0) {
+			System.out.println(root.data);
+		}
+		printAtDepthK(root.left, k-1);
+		printAtDepthK(root.right, k-1);
+	}
