23f2b0c0e7b14f28cb9ad82a904ae5dffcbcdca8
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-17 17:26:43 +0530

    Update changeToDepth.java

--- Tree-1/changeToDepth.java
+++ Tree-1/changeToDepth.java
@@ -1,1 +1,13 @@
 
+public static void changeToDepth(Node  root, int level) {
+		if(root == null) {
+			return;
+		}
+		
+		root.data = level;
+		
+		changeToDepth(root.left,  level+1);
+		changeToDepth(root.right, level+1);
+		
+		
+	}
