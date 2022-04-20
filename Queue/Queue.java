f9a0e76a280c960535211114ddc6236db95a5cab
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-20 23:46:54 +0530

    Update Queue.java

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,1 +1,13 @@
+package stack;
+
+
+import java.util.LinkedList;
+public class Stack {
+	public static void main(String[] args) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(10);
+		System.out.println(queue.size());
+
+	}
+}
 
