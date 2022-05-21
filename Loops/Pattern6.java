cc54be9c9e4f0e9df5a217a70d05d51ee5dfd8c2
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-22 00:07:05 +0530

    Update Pattern6.java

--- Loops/Pattern6.java
+++ Loops/Pattern6.java
@@ -1,1 +1,20 @@
 
+package loops;
+
+public class Pattern6 {
+	public static void main(String[] hitesh) {
+		int j=4;
+		while(j>0) {
+			int i=4;
+			while(i>=j) {
+				char output=(char)('A'+j-1);
+				System.out.print(output);
+				i--;
+				
+			}
+			j--;
+			System.out.println();
+		}
+	}
+
+}
