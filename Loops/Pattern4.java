54202f73554747eea98597e130c6547d3e217c8b
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-21 23:47:57 +0530

    Update Pattern4.java

--- Loops/Pattern4.java
+++ Loops/Pattern4.java
@@ -1,1 +1,20 @@
 
+package loops;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		
+		int i=1;
+		while(i<5) {
+			int j=1;
+			while(j<=i) {
+				char output = (char)('A'+i-1);
+				System.out.print(output+" ");
+				j+=1;
+			}
+			i+=1;
+			System.out.println();
+		}
+	}
+
+}
