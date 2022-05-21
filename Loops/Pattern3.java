ce8da37be6889b07efa7aa23f42e4e5614f6862c
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-21 23:44:43 +0530

    Update Pattern3.java

--- Loops/Pattern3.java
+++ Loops/Pattern3.java
@@ -1,1 +1,20 @@
 
+package loops;
+
+public class Pattern3 {
+	public static void main(String[] args) {
+		
+		int i=1;
+		while(i<5) {
+			int j=1;
+			while(j<5) {
+				char output = (char)('A'+j-1);
+				System.out.print(output+" ");
+				j+=1;
+			}
+			i+=1;
+			System.out.println();
+		}
+	}
+
+}
