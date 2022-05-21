998e09890b7b432a3a6f4007c4e958f9ad8aebcc
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-21 23:37:44 +0530

    Update Pattern2.java

--- Loops/Pattern2.java
+++ Loops/Pattern2.java
@@ -1,1 +1,19 @@
 
+package loops;
+
+public class Pattern2 {
+	public static void main(String[] args) {
+		int i= 1;
+		while(i<=5) {
+			int j=1;
+			int p=i;
+			while(j<=i) {
+				System.out.print(p);
+				p+=1;
+				j+=1;
+			}
+			i+=1;
+			System.out.println();
+		}
+	}
+}
