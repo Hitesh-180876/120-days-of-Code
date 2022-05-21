7c1efe5c3e791a1c1c874ab94c3d8314c5751919
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-22 00:53:17 +0530

    Update SumOfElements.java

--- Arrays/SumOfElements.java
+++ Arrays/SumOfElements.java
@@ -1,1 +1,24 @@
 
+package arraypackage;
+
+public class SumOfElements {
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 6, 6, 7, 8};
+		
+		int sum = 0;
+		int i = arr.length;
+		while(i>0) {
+			sum+=arr[i-1];
+			i--;
+		}
+		System.out.println(sum);
+		int sum1 =0;
+		
+		for(int a=0; a<arr.length; a++) {
+			sum1+=arr[a];
+		}
+		System.out.println(sum1);
+	}
+	
+
+}
