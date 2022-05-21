38d915d45b9c81582133e6fab678ba62ec17e725
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-21 23:55:13 +0530

    Update Pattern5.java

--- Loops/Pattern5.java
+++ Loops/Pattern5.java
@@ -1,1 +1,22 @@
 
+package loops;
+
+public class Pattern4 {
+	public static void main(String[] Hitesh) {
+		int i=1;
+		while(i<5) {
+			int p=i;
+			int j=1;
+			while(j<5) {
+				char output = (char)('A'+p-1);
+				System.out.print(output);
+				j+=1;
+				p+=1;
+				
+			}
+			i+=1;
+			System.out.println();
+		}
+	}
+
+}
