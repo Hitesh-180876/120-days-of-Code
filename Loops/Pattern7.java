29b26d25c91c0265d9cb6c3203f34734d856ca28
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-22 00:14:42 +0530

    Update Pattern7.java

--- Loops/Pattern7.java
+++ Loops/Pattern7.java
@@ -1,1 +1,22 @@
 
+package loops;
+
+public class Pattern7 {
+	public static void main(String[] hitesh) {
+		int i=1;
+		
+		while(i<=4) {
+			int j=1;
+			char output = (char)('A'+4-i);
+			while(j<=i) {
+				System.out.print(output);
+				j+=1;
+				output+=1;
+			}
+			i++;
+			System.out.println();
+		}
+		
+		
+	}
+}
