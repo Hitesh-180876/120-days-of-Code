3d22c2ce2321e372391ffa330ed800df6e26aa08
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-22 00:40:52 +0530

    Update PostPreOperators.java

--- Loops/PostPreOperators.java
+++ Loops/PostPreOperators.java
@@ -1,1 +1,17 @@
 
+package loops;
+
+public class PostPreOperators {
+	public static void main(String[] hitesh) {
+		int i=1;
+		int j=1;
+		int k=1;
+		int l=1;
+		System.out.println(i++ +""+ j++);
+		System.out.println(k-- +""+ l--);
+		System.out.println(--i +""+ --j);
+		System.out.println(++i +""+ ++j);
+		
+	}
+
+}
