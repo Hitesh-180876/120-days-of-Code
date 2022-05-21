10ea56e6fb887e5d58491ff107ee55d990157be8
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-21 23:31:45 +0530

    Update Pattern1.java

--- Loops/Pattern1.java
+++ Loops/Pattern1.java
@@ -1,1 +1,19 @@
 
+package loops;
+
+public class Pattern1 {
+	public static void main(String[] abc) {
+		int p=1;
+		int i = 0;
+		while(i<5) {
+			int j=0;
+			while(j<=i) {
+				System.out.print(p+" ");
+				p+=1;
+				j+=1;
+			}
+			i+=1;
+			System.out.println();
+		}	
+	}
+}
