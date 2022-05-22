a39e597eb68f7a80b950f6583d5839b9a44e0908
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-05-23 00:38:09 +0530

    Update BinarySearch.java

--- 2. Searching/BinarySearch.java
+++ 2. Searching/BinarySearch.java
@@ -1,1 +1,33 @@
 
+package searchingAndSorting;
+
+public class BinarySearch {
+	public static int binarySearch(int arr[], int x) {
+		int left = arr[0];
+		int right = arr[arr.length-1];
+		
+		while(left<=right) {
+			int mid = left+(right-left)/2;
+			
+			if(arr[mid] > x) {
+				right=mid-1;
+			}
+			
+			else if(arr[mid]<x) {
+				left = mid+1;
+			}
+			
+			else return mid;
+			
+		}
+		return -1;
+		
+	}
+	public static void main(String[] hitesh) {
+		int arr[] = {1, 2, 3, 4, 5, 6, 8};
+		int a = 5;
+		int bs = binarySearch(arr, a);
+		System.out.println(bs);
+	}
+
+}
