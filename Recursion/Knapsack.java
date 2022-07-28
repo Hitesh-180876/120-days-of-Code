8cbf61a678172ec8cc605874ae95194af25614ec
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-07-28 18:19:31 +0530

    Update Knapsack.java

--- Recursion/Knapsack.java
+++ Recursion/Knapsack.java
@@ -1,1 +1,31 @@
 
+package recursion;
+
+public class Knapsack {
+	static int maxWeight(int[] val,  int[] weight, int W, int i) {
+		
+		if(i == val.length) {
+			return 0;
+		}
+		int ans;
+		if(weight[i]<=W) {
+			int ans1 = maxWeight(val, weight, W, i+1);
+			int ans2 = val[i]+maxWeight(val, weight, W-weight[i], i+1);
+			
+			ans = Math.max(ans1, ans2);
+		}else {
+			ans = maxWeight(val, weight, W, i+1);
+		}
+		
+		return ans;
+	}
+	public static void main(String[] args) {
+		int[] val = {60, 100, 120};
+		int[] weight = {10, 20, 30};
+		int W = 50;
+		
+		int ans = maxWeight(val, weight, W, 0);
+		System.out.print(ans);
+	}
+
+}
