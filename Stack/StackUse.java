75175a259eec20996646044eb396f33c6fd09223
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-09-01 18:20:41 +0530

    Update StackUse.java

--- Stack/StackUse.java
+++ Stack/StackUse.java
@@ -1,1 +1,18 @@
 
+package stack;
+
+public class StackUse {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello world");
+		StackUsingArray s = new StackUsingArray();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		
+	}
+
+}
