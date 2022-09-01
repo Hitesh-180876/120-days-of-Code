5999b036aedb8ec4c1a26a7019c7e6d16ee8dfd1
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-09-01 11:21:55 +0530

    Update 10.findDuplicatesInArray.java

--- Arrays/10.findDuplicatesInArray.java
+++ Arrays/10.findDuplicatesInArray.java
@@ -25,3 +25,57 @@ public class DupInArray {
 		
 	}
 }
+
+/* Another solution can be */
+
+package test1;
+
+public class Test {
+	public static int sum(int[] arr, int ele) {
+		int sum1 = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum1 = sum1+arr[i];
+		}
+		int a = sum1-ele;
+		
+		int n = arr.length;
+		int sum = n*(n+1)/2;
+		
+		int ans = sum - a;
+		
+		return ans;
+		
+		
+	}
+	public static int duplicateEle(int[] arr) {
+		int n = arr.length;
+		int count[] = new int[n+1];
+		
+		for(int i=0; i<arr.length; i++) {
+			if(count[arr[i]] == 0) {
+				count[arr[i]] += 1;
+				
+			}else {
+				return arr[i];
+			}
+		}
+		
+		return Integer.MIN_VALUE;
+	}
+	public static void main(String[] args) {
+		/*
+		 * String s = "AABBBFFEDA";
+		 * 
+		 * for(int i=0; i<s.length(); i++) { int count = 1; while(i+1 < s.length() &&
+		 * s.charAt(i) == s.charAt(i+1)) { count++; i++; }
+		 * System.out.print(s.charAt(i)+""+count); }
+		 */
+		
+		int arr[]  = {1, 2, 3, 4, 4};
+		int ele = duplicateEle(arr);
+		System.out.println(ele);
+		int ans = sum(arr, ele);
+		System.out.println(ans);
+	}
+}
+
